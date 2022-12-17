de22d5fef03182b58552a13c3be3e0625fd7e5ca
Noah Nelson <nomnelson@icloud.com>
2022-12-17 13:33:55 -0600

    updated user controller

--- Backend/src/main/java/backend/user/UserController.java
+++ Backend/src/main/java/backend/user/UserController.java
@@ -1,41 +1,21 @@
-package backend.Users;
+package backend.user;
 
 
-import backend.Items.Item;
-import backend.Items.ItemRepository;
-import backend.Ratings.Rating;
-import backend.Ratings.RatingRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.sql.rowset.serial.SerialBlob;
-import java.io.IOException;
-import java.sql.Blob;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
  * The controller for the User class
  */
 @RestController
+@CrossOrigin
 public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    ItemRepository itemRepository;
-
-    @Autowired
-    RatingRepository ratingRepository;
-
-    private String success = "{\"message\":\"success\"}";
+    private final String success = "{\"message\":\"success\"}";
     private String failure = "{\"message\":\"failure\"}";
 
     /**
@@ -64,6 +44,7 @@ public class UserController {
      */
     @GetMapping(path = "/users/username/{userName}")
     User getUserByUserName(@PathVariable String userName) {
+        System.out.println(userRepository.findByUserName(userName).getId());
         return userRepository.findByUserName(userName);
     }
 
@@ -72,7 +53,8 @@ public class UserController {
      * @param user
      * @return fail or success
      */
-    @PostMapping(path = "/users")
+    @RequestMapping(value = "/users", produces = "application/json", method = RequestMethod.POST)
+    @ResponseBody
     String createUser(@RequestBody User user){
         if (user == null)
             return failure;
@@ -92,47 +74,10 @@ public class UserController {
         if(user == null)
             return null;
         request.setId(id);
-        request.setItems(user.getItems());
         userRepository.save(request);
         return userRepository.findById(id);
     }
 
-    /**
-     * Method used to add an item to a user
-     * @param userId the userID
-     * @param itemID the itemID
-     * @return fail or success
-     */
-    String assignItemToUser(int userId, int itemID){
-        User user = userRepository.findById(userId);
-        Item item = itemRepository.findById(itemID);
-        if(user == null || item == null)
-            return failure;
-        item.setUser(user);
-        user.addItem(item);
-        item.setUserID(userId);
-        userRepository.save(user);
-        itemRepository.save(item);
-        return success;
-    }
-
-    /**
-     * Method used to create an item
-     * @param userId the user ID of the item owner
-     * @param item the Item in form JSON
-     * @return fail or success
-     */
-    @PostMapping("/users/{userId}/items")
-    String createItem(@PathVariable int userId, @RequestBody Item item) {
-        User user = userRepository.findById(userId);
-        if (item == null)
-            return failure;
-        item.setUserID(userId);
-        item.setCity(user.getCity());
-        item.setState(user.getState());
-        itemRepository.save(item);
-        return assignItemToUser(userId, item.getId());
-    }
 
     /**
      * Method used for deleting a user, will also delete all items tied to user
@@ -142,63 +87,8 @@ public class UserController {
     @DeleteMapping(path = "/users/{id}")
     String deleteUser(@PathVariable int id){
         User user = userRepository.findById(id);
-        for (int x = 0; x < user.getItems().size(); x++) {
-            itemRepository.deleteById(user.getItems().get(x).getId());
-        }
         userRepository.deleteById(id);
         return success;
     }
-
-    @PostMapping(path = "/users/rating/{userID}/{reviewersID}/{rating}/{reviewText}")
-    String addRatringWithReview(@PathVariable("userID") int userID, @PathVariable("reviewersID") int reviewersID, @PathVariable("rating") int rating, @PathVariable("reviewText") String review) {
-        User user = userRepository.findById(userID);
-        User reviewer = userRepository.findById(reviewersID);
-        if (review == null) {
-            Rating newReview = new Rating(rating, reviewersID);
-            user.addReview(newReview);
-            newReview.setUser(user);
-            newReview.setUserGiving(reviewer);
-            ratingRepository.save(newReview);
-
-        } else {
-            Rating newReview = new Rating(rating, review,reviewersID);
-            user.addReview(newReview);
-            newReview.setUser(user);
-            newReview.setUserGiving(reviewer);
-            ratingRepository.save(newReview);
-        }
-        int avg = 0;
-        for (int x = 0; x < user.getReviewsGotten().size(); x++) {
-            avg += user.getReviewsGotten().get(x).getRating();
-        }
-        avg /= user.getReviewsGotten().size();
-        user.setAverageRating(avg);
-        userRepository.save(user);
-        return success;
-    }
-
-    @PostMapping(path = "/users/rating/{userID}/{reviewersID}/{rating}")
-    String addRatingNoReview(@PathVariable("userID") int userID, @PathVariable("reviewersID") int reviewersID, @PathVariable("rating") int rating) {
-        User user = userRepository.findById(userID);
-        User reviewer = userRepository.findById(reviewersID);
-        Rating newReview = new Rating(rating,reviewersID);
-        user.addReview(newReview);
-        newReview.setUser(user);
-        newReview.setUserGiving(reviewer);
-        ratingRepository.save(newReview);
-
-        int avg = 0;
-        for (int x = 0; x < user.getReviewsGotten().size(); x++) {
-            avg += user.getReviewsGotten().get(x).getRating();
-        }
-        avg /= user.getReviewsGotten().size();
-        user.setAverageRating(avg);
-        userRepository.save(user);
-        return success;
-    }
-
-
-
-
 
 }
--- Backend/src/main/java/backend/user/UserRepository.java
+++ Backend/src/main/java/backend/user/UserRepository.java
@@ -1,4 +1,4 @@
-package backend.Users;
+package backend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
@@ -6,7 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(int id);
+
     User findByUserName(String userName);
+
     @Transactional
     void deleteById(int id);
 }
